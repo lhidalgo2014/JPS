b0d569e0f3dfd724b6610391e3a529a522c1e45c
leister francisco alvarez campos <leisterac.1997@gmail.com>
2020-12-16 19:18:50 -0600

    third commit

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ ReactDOM.render(
 );
 
 // comentario de prueba
+// comentario 2 de prueba
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
