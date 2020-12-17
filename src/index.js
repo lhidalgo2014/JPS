c0d679a27f016f35d19f36cfebe5092921446890
leister francisco alvarez campos <leisterac.1997@gmail.com>
2020-12-16 19:20:19 -0600

    comentario de gps

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ ReactDOM.render(
 );
 
 // comentario de prueba
+// comentario de gps
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
